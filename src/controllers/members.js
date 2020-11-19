62f8d05f5c95d081dff59b6943dbff5081c6cebd
Indra Pramana <indra@ide.web.id>
2020-11-19 16:38:20 +0700

    forgot findAll in member.js -v1

--- src/controllers/members.js
+++ src/controllers/members.js
@@ -2,7 +2,8 @@ const e = require('express')
 const Member = require('../models/member')
 
 exports.listMembers = (req, res, next) => {
-    const policyId = req.params.policy
+    const policyId = req.params.policyId
+    
     Member.findAll({ where: { policy_id: policyId, is_active: 1 }})
         .then(results => {
             res.status(200).json({ success: true, data: results })
