b5ba447b519bd80c0d56d78715a0a44c90755f4f
Indra Pramana <indra@ide.web.id>
2020-11-23 09:24:27 +0700

    include insurance in controllerusers -rev 1

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,7 +1,7 @@
 const User = require('../models/user')
 const Client = require('../models/client')
 const Policy = require('../models/policy')
-const Insurance = require('../models/Insurance')
+const Insurance = require('../models/insurance')
 
 // show all users
 exports.listUsers = (req, res, next) => {
