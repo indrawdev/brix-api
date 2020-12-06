8f03a6b0b82b8abd25a811d881690eeb33b56cff
indrapramana <idewebid@gmail.com>
2020-12-06 13:36:51 +0700

    add route front

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -122,3 +122,13 @@ exports.signOut = (req, res, next) => {
     }
     
 }
+
+exports.front = (req, res, next) => { 
+    res.status(200).json({
+        error: false,
+        data: {
+            author: 'IT Development',
+            message: 'Consuming API, please contact it@integra.co.id'
+        }
+    })
+}
